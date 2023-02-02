957507d12dd898c6f9919008b47d04bfa97a1a50
ROUVELL <sheva15032005@gmail.com>
2023-02-02 21:21:46 +0100

    group in init and kill method

--- ball.py
+++ ball.py
@@ -4,18 +4,20 @@ from config import *
 
 
 class Ball:
-    def __init__(self, direction):
+    def __init__(self, direction, pos=BALL_START_POS, *, group):
+        self.group = group
+        ##########
         self.rect = pg.Rect((0, 0), (BALL_DIAMETR, BALL_DIAMETR))
-        self.rect.center = BALL_START_POS
+        self.rect.center = pos
         self.color = 'red'
         self.direction = pg.Vector2(direction)
+
+    def kill(self):
+        self.group.remove(self)
 
     def change_direction(self, *, x=False, y=False):
         if x: self.direction.x = -self.direction.x
         if y: self.direction.y = -self.direction.y
-
-    def change_speed(self, sx=0, sy=0):
-        self.direction += pg.Vector2(sx, sy)
 
     def update(self):
         self.rect.move_ip(self.direction)
