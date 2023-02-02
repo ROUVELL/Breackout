dd77aa8203663ac1a2ad8e81a0f306d90579292b
ROUVELL <sheva15032005@gmail.com>
2023-02-02 20:08:13 +0100

    brick and level configs delete

--- config.py
+++ config.py
@@ -6,15 +6,6 @@ user = ctypes.windll.user32
 WIDTH, HEIGHT = user.GetSystemMetrics(78), user.GetSystemMetrics(79)  # fullscreen
 SCREEN = (WIDTH, HEIGHT)
 
-# brick
-BRICK_WIDTH = 100
-BRICK_HIGHT = 50
-BRICK_SIZE = (BRICK_WIDTH, BRICK_HIGHT)
-
-# level
-MAX_COLUMNS = (WIDTH // BRICK_WIDTH)
-MAX_ROWS = (HEIGHT // BRICK_HIGHT) - 3
-
 # padle
 PADLE_WIDTH = 120
 PADLE_HEIGHT = 20
@@ -24,3 +15,7 @@ PADLE_POS = (WIDTH // 2, HEIGHT - PADLE_HEIGHT - 5)
 # ball
 BALL_DIAMETR = 16
 BALL_START_POS = (WIDTH // 2, 50)
+
+# colors
+BG = (10, 10, 10)
+BRICK_COLORS = ['orange', 'lightgreen', 'lightgrey', 'azure', 'skyblue', 'pink', 'brown', 'yellow']
