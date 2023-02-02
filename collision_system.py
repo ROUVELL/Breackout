0f5c60c505de15c0c812cc8d2f3fe42d7a64b38b
ROUVELL <sheva15032005@gmail.com>
2023-02-02 21:22:17 +0100

    some updates

--- collision_system.py
+++ collision_system.py
@@ -4,51 +4,49 @@ from config import WIDTH, HEIGHT
 
 
 class CollisionSystem:
-    def __init__(self, padle, ball, bricks):
+    def __init__(self, padle, balls, bricks):
         self.padle = padle
-        self.ball = ball
+        self.balls = balls
         self.bricks = bricks
 
-    def _ball_with_bricks(self) -> pg.Vector2:
+    def _ball_with_bricks(self, ball) -> pg.Vector2:
         colliders = self.bricks.copy()
-        dx, dy = self.ball.direction
+
+        dx, dy = ball.direction
 
         # vertical and horizontal collision
-        x_indexes = self.ball.rect.move(dx, 0).collidelistall(colliders)
-        y_indexes = self.ball.rect.move(0, dy).collidelistall(colliders)
+        x_indexes = ball.rect.move(dx, 0).collidelistall(colliders)
+        y_indexes = ball.rect.move(0, dy).collidelistall(colliders)
 
         if x_indexes:
-            self.ball.change_direction(x=True)
+            ball.change_direction(x=True)
         if y_indexes:
-            self.ball.change_direction(y=True)
+            ball.change_direction(y=True)
 
         # delete brick(s)
         [colliders[i].kill() for i in {*x_indexes, *y_indexes}]
 
-
-    def _ball_with_padle(self):
+    def _ball_with_padle(self, ball):
         # TODO: Зміна напряму м'яча в залежності від дистанції до центру дошки
 
-        is_collide = lambda dx, dy: self.ball.rect.move(dx, dy).colliderect(self.padle)
+        is_collide = lambda dx, dy: ball.rect.move(dx, dy).colliderect(self.padle)
 
-        dx, dy = self.ball.direction
+        dx, dy = ball.direction
         if is_collide(dx, 0):
-            self.ball.change_direction(x=True)
+            ball.change_direction(x=True)
         if is_collide(0, dy):
-            self.ball.change_direction(y=True)
+            ball.change_direction(y=True)
 
-    def _ball_with_screen(self):
-        if self.ball.rect.top < 0:
-            self.ball.direction.y = -self.ball.direction.y
-        if self.ball.rect.left < 0 or self.ball.rect.right > WIDTH:
-            self.ball.direction.x = -self.ball.direction.x
+    def _ball_with_screen(self, ball):
+        if ball.rect.top < 0:
+            ball.change_direction(y=True)
+        if ball.rect.left < 0 or ball.rect.right > WIDTH:
+            ball.change_direction(x=True)
+        if ball.rect.top > HEIGHT:
+            ball.kill()
 
-    def ball_with_bottom(self) -> bool:
-        if self.ball.rect.top > HEIGHT:
-            return True
-        return False
-
-    def ball_with_all(self):
-        self._ball_with_bricks()
-        self._ball_with_screen()
-        self._ball_with_padle()
+    def balls_with_all(self):
+        for ball in self.balls.copy():
+            self._ball_with_bricks(ball)
+            self._ball_with_padle(ball)
+            self._ball_with_screen(ball)
