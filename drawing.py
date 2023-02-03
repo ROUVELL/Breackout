0e55e2537ac208b3e1f3090d72f937af45b39fb3
ROUVELL <sheva15032005@gmail.com>
2023-02-03 22:25:17 +0100

    update text drawing

--- drawing.py
+++ drawing.py
@@ -3,12 +3,13 @@ import pygame as pg
 from padle import Padle
 from group import Group
 from timer import TimerGroup
-from config import BG, CENTER
+from config import BG, CENTER, HALF_WIDTH, HALF_HEIGHT
 
 
 class Drawing:
-    def __init__(self, clock: pg.time.Clock, padle: Padle, bricks: Group, balls: Group, timers: TimerGroup):
+    def __init__(self, breakout, clock: pg.time.Clock, padle: Padle, bricks: Group, balls: Group, timers: TimerGroup):
         self._sc = pg.display.get_surface()
+        self._breakout = breakout
         self._clock = clock
         #######
         self._padle = padle
@@ -19,20 +20,28 @@ class Drawing:
         self._font = pg.font.SysFont('calibri', 26)
         self._font2 = pg.font.SysFont('calibri', 36)
 
+    def _text(self, text: str, pos: tuple | pg.Rect, font: pg.font.FontType, *, centralize=False):
+        render = font.render(text, True, 'white')
+        if centralize:
+            self._sc.blit(render, render.get_rect(center=pos))
+            return
+        self._sc.blit(render, pos)
+
     def _time_to_restart(self):
         if self._timers.is_active('restart_timer'):
+            if self._breakout.is_win:
+                self._text('VICTORY!', CENTER, self._font2, centralize=True)
+            if self._breakout.is_loss:
+                self._text('DEFEAT!', CENTER, self._font2, centralize=True)
             time = f"{self._timers.get_remaining_time('restart_timer') / 1000 : .2f}"
-            render = self._font2.render(f'Time to restart: {time}', True, 'white')
-            self._sc.blit(render, render.get_rect(center=CENTER))
+            self._text(f'Time to restart: {time}', (HALF_WIDTH, HALF_HEIGHT + 26), self._font, centralize=True)
 
     def _bricks_count(self):
-        render = self._font.render(f'Bricks: {len(self._bricks)}', True, 'grey')
-        self._sc.blit(render, (80, 0))
+        self._text(f'Bricks: {len(self._bricks)}', (80, 0), self._font)
 
     def _fps(self):
         fps = f'{self._clock.get_fps(): .1f}'
-        text = self._font.render(fps, True, 'grey')
-        self._sc.blit(text, (0, 0))
+        self._text(fps, (0, 0), self._font)
 
     def all(self):
         self._sc.fill(BG)
