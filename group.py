659cbdfe859a5f53dda8dfb43d392da1f7c14066
ROUVELL <sheva15032005@gmail.com>
2023-02-03 22:02:13 +0100

    items now is private attr

--- group.py
+++ group.py
@@ -3,22 +3,28 @@ import pygame as pg
 
 class Group:
     def __init__(self, *items):
-        self.items = [*items]
+        self._items = [*items]
 
     def __len__(self):
-        return len(self.items)
+        return len(self._items)
+
+    @property
+    def is_empty(self): return not len(self._items)
+
+    def clear(self):
+        self._items.clear()
 
     def copy(self):
-        return self.items[:]
+        return self._items[:]
 
     def add(self, *items):
-        [self.items.append(item) for item in items]
+        [self._items.append(item) for item in items]
 
     def remove(self, item):
-        self.items.remove(item)
+        self._items.remove(item)
 
     def update(self, *args, **kwargs):
-        [item.update(*args, **kwargs) for item in self.items]
+        [item.update(*args, **kwargs) for item in self._items]
 
     def draw(self, sc: pg.Surface):
-        [item.draw(sc) for item in self.items]
+        [item.draw(sc) for item in self._items]
