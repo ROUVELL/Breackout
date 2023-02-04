13fc5204f7546f39737122c3a19c05c383308604
ROUVELL <sheva15032005@gmail.com>
2023-02-05 00:46:36 +0200

    refactoring

--- ball.py
+++ ball.py
@@ -8,15 +8,13 @@ class Ball:
     def __init__(self, direction: tuple, pos: tuple = BALL_START_POS, *, group: Group):
         self.group = group
         ##########
-        self.rect = pg.Rect((0, 0), (BALL_DIAMETR, BALL_DIAMETR))
-        self.rect.center = pos
-        self.color = 'red'
+        self.rect = pg.Rect(pos, (BALL_DIAMETR, BALL_DIAMETR))
         self.direction = pg.Vector2(direction)
 
     def kill(self):
         self.group.remove(self)
 
-    def change_direction(self, *, x=False, y=False):
+    def change_direction(self, *, x=None, y=None):
         if x: self.direction.x = -self.direction.x
         if y: self.direction.y = -self.direction.y
 
@@ -24,4 +22,4 @@ class Ball:
         self.rect.move_ip(self.direction * dt)
 
     def draw(self, sc: pg.Surface):
-        pg.draw.circle(sc, self.color, self.rect.center, BALL_DIAMETR // 2)
+        pg.draw.circle(sc, 'red', self.rect.center, BALL_DIAMETR // 2)
--- padle.py
+++ padle.py
@@ -7,7 +7,6 @@ class Padle:
     def __init__(self):
         self.rect = pg.Rect((0, 0), PADLE_SIZE)
         self.rect.center = PADLE_POS
-        self.color = 'white'
         self.speed = PADLE_SPEED
 
     def update(self, dt: float):
@@ -16,4 +15,4 @@ class Padle:
         if keys[pg.K_d]: self.rect.move_ip(self.speed * dt, 0)
 
     def draw(self, sc: pg.Surface):
-        pg.draw.rect(sc, self.color, self.rect, border_radius=5)
+        pg.draw.rect(sc, 'white', self.rect, border_radius=5)
